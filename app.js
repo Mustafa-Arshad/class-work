0bc553300ab69be685e4c5096dc059457059a4f6
Mustafa-Arshad <hafiz28mustafa@gmail.com>
2022-11-14 07:17:58 -0800

    done

---
+++ app.js
@@ -0,0 +1,25 @@
+// var a = "Karachi";
+
+// var firstletter = a.slice(0, 1)
+// var othertletter = a.slice(1)
+
+// a = firstletter.toUpperCase() + othertletter.toLowerCase();
+// console.log(a)
+
+ var a ="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam placeat minus perferendis non dolore ratione sunt dignissimos. Dicta neque, voluptate voluptatem, quos nostrum quidem fugit dolore tempore quisquam perferendis nisi Sed, enim, reiciendis ipsa officia exercitationem rerum eligendi adipisci sapiente ut hic debitis sequi in nemo ducimus dolorum suscipit autem id repudiandae omnis laboriosam soluta. Porro cumque blanditiis excepturi tenetur"
+
+ var b ="amet"
+
+ for(i = 0; i < a.length; i++){
+    var word = a.slice(i, i + 4);
+ }
+ if(word == b){
+    console.log("found", word);
+    console.log(i);
+
+    var starttingletters = a.slice(0, i);
+    var myword ="ametment";
+    var starttingletters = a.slice(i, 4);
+    
+    a = starttingletters + myword + otherlatters;
+ }
